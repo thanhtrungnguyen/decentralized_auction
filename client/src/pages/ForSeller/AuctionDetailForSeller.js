2af70de828bad488d17c785f772f7cbac3e9ac85
An Thuyen <anthuyen0206@gmail.com>
2022-11-23 22:08:05 +0700

    commit dropdown

---
+++ client/src/pages/ForSeller/AuctionDetailForSeller.js
@@ -0,0 +1,30 @@
+import Header from "../../components/header/HeaderUser";
+import NavBar from "../../components/navbar/NavBar";
+import Footer from "../../components/footer/Footer";
+import styles from "../../styleCss/stylesPages/forSellers/AuctionDetailForSeller.module.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import useFetch from "../../hook/useFetch";
+import SideBarSeller from "../../components/sidebar_seller/SidebarSeller";
+
+const AuctionDetailForSeller = () => {
+  const { id } = useParams();
+  const baseURL = `http://localhost:8800/api/auction/auctiondetail/${id}`;
+  const { data, loading, error } = useFetch(baseURL);
+  console.log(data);
+  console.log(loading);
+  return loading ? (
+    "loading please wait"
+  ) : (
+    <>
+      <div>
+        <Header />
+        <NavBar />
+        <SideBarSeller />
+        <Footer />
+      </div>
+    </>
+  );
+};
+export default AuctionDetailForSeller;
