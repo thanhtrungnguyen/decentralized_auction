a6246b8bd7e01529bf0c41adfacf87bbbcc0639a
congtm20 <congtm20@gmail.com>
2022-12-21 14:40:52 +0700

    refactor conn

--- server/dal/NewsDAO.js
+++ server/dal/NewsDAO.js
@@ -1,15 +1,33 @@
 const conn = require('./connectSF')
 const perPage = 10;
 
-const getAll = async (index) => {
+const getAll = async (index, status, title) => {
     try {
+        var connection = await conn();
         var listNews, total, totalNews = null;
         var num = (parseInt(index) - 1) * perPage;
-        await conn.query(`Select Id, Name,Status__c, CreatedDate,LastModifiedDate from News_DAP__c order by CreatedDate desc limit ${perPage} offset ${num} `, (err, result) => {
-            if (err) console.log(err)
-            listNews = result.records
-        })
-        await conn.query(`Select Id from News_DAP__c`, (err, result) => {
+        if (status == '' && title == '') {
+            await connection.query(`Select Id, Name,Status__c, CreatedDate,LastModifiedDate from News_DAP__c order by CreatedDate desc limit ${perPage} offset ${num} `, (err, result) => {
+                if (err) console.log(err)
+                listNews = result.records
+            })
+        } else if (status == '') {
+            await connection.query(`Select Id, Name,Status__c, CreatedDate,LastModifiedDate from News_DAP__c where Name like '%${title}%' order by CreatedDate desc  limit ${perPage} offset ${num} `, (err, result) => {
+                if (err) console.log(err)
+                listNews = result.records
+            })
+        } else if (title == '') {
+            await connection.query(`Select Id, Name,Status__c, CreatedDate,LastModifiedDate from News_DAP__c where Status__c = '${status}' order by CreatedDate desc  limit ${perPage} offset ${num} `, (err, result) => {
+                if (err) console.log(err)
+                listNews = result.records
+            })
+        } else {
+            await connection.query(`Select Id, Name,Status__c, CreatedDate,LastModifiedDate from News_DAP__c where Status__c = '${status}' and Name like '%${title}%' order by CreatedDate desc  limit ${perPage} offset ${num} `, (err, result) => {
+                if (err) console.log(err)
+                listNews = result.records
+            })
+        }
+        await connection.query(`Select Id from News_DAP__c`, (err, result) => {
             if (err) console.log(err)
             total = result;
             totalNews = total.totalSize;
@@ -21,9 +39,10 @@ const getAll = async (index) => {
 }
 const getByStatus = async (index, status) => {
     try {
+        var connection = await conn();
         var listNews, total, totalNews = null;
         var num = (parseInt(index) - 1) * perPage;
-        await conn.query(`Select Id, Name,Status__c from News_DAP__c where Status__c = '${status}' order by CreatedDate desc limit ${perPage} offset ${num}`, (err, result) => {
+        await connection.query(`Select Id, Name,Status__c from News_DAP__c where Status__c = '${status}' order by CreatedDate desc limit ${perPage} offset ${num}`, (err, result) => {
             if (err) console.log(err)
             listNews = result
         })
@@ -37,17 +56,17 @@ const getByStatus = async (index, status) => {
         console.error(error)
     }
 }
-const getById = async (newsId)=>{
+const getById = async (newsId) => {
 
 }
-const filter = async (title,index,status)=>{
+const filter = async (title, index, status) => {
     try {
-        var listNews,total, totalNews = null;
-        var num = (parseInt(index) - 1)*perPage;
-        await conn.query(`Select Id, Name,Status__c from News_DAP__c where Name like '%${title}%' And Status__c = '${status}' order by CreatedDate desc limit ${perPage} offset ${num}`,(err, result) => {
+        var listNews, total, totalNews = null;
+        var num = (parseInt(index) - 1) * perPage;
+        await conn.query(`Select Id, Name,Status__c from News_DAP__c where Name like '%${title}%' And Status__c = '${status}' order by CreatedDate desc limit ${perPage} offset ${num}`, (err, result) => {
             if (err) console.log(err)
             listNews = result
-        })                
+        })
         await conn.query(`Select Id from News_DAP__c`, (err, result) => {
             if (err) console.log(err)
             total = result;
@@ -58,13 +77,14 @@ const filter = async (title,index,status)=>{
         console.error(error)
     }
 }
-const changeStatus = async (id,status)=>{
+const changeStatus = async (id, status) => {
     try {
-       const changedStatus =  await conn.sobject("News_DAP__c")
+        var connection = await conn();
+        const changedStatus = await connection.sobject("News_DAP__c")
             .find({ Id: id })
             .update({
                 Status__c: status
-            },(err, result) => {
+            }, (err, result) => {
                 if (err) console.log(err)
             })
         return changedStatus;
@@ -72,25 +92,27 @@ const changeStatus = async (id,status)=>{
         console.error(error)
     }
 }
-const update = async (id,title,description,status)=>{
+const update = async (id, title, description, status) => {
     try {
-        var updated = await conn.sobject("News_DAP__c")
-                .find({ Id: id })
-                .update({
-                    Name: title,
-                    Description__c: description,
-                    Status__c: status
-                },(err, result) => {
-                    if (err) console.log(err)
-                })
-                return updated;
+        var connection = await conn();
+        var updated = await connection.sobject("News_DAP__c")
+            .find({ Id: id })
+            .update({
+                Name: title,
+                Description__c: description,
+                Status__c: status
+            }, (err, result) => {
+                if (err) console.log(err)
+            })
+        return updated;
     } catch (error) {
         console.error(error)
     }
 }
-const create = async (title,description)=>{
+const create = async (title, description) => {
     try {
-        var created = await conn.sobject("News_DAP__c").create({
+        var connection = await conn();
+        var created = await connection.sobject("News_DAP__c").create({
             Name: title,
             Description__c: description,
             Status__c: 'Published'
@@ -102,5 +124,5 @@ const create = async (title,description)=>{
         console.error(error)
     }
 }
-module.exports = { getAll, getByStatus,filter,changeStatus,update,create,getById }
+module.exports = { getAll, getByStatus, filter, changeStatus, update, create, getById }
 
