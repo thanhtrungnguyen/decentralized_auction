ca411ca96867774460d53ff67a660df268b5aa07
Nguyen Thanh Trung <thanhtrung692k@gmail.com>
2022-12-23 00:43:34 +0700

    fix

--- client/src/pages/forAdmin/AddManager.js
+++ client/src/pages/forAdmin/AddManager.js
@@ -87,7 +87,7 @@ const AddManager = () => {
                                 id="userName"
                                 type="text"
                                 className={styles.input}
-                                value={userName}
+                                value={username}
                                 onChange={(e) => handleInputChange(e)}
                                 required
                             ></input>
