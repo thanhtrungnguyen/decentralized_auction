ab3e15f1ab23fdb9e457173d9b4e4c86620857a9
Nguyen Thanh Trung <thanhtrung692k@gmail.com>
2023-01-05 22:18:08 +0700

    jwt

--- server/src/api/controllers/SessionController.ts
+++ server/src/api/controllers/SessionController.ts
@@ -20,6 +20,7 @@ const createUserSessionHandler = async (req: Request, res: Response, next: NextF
 };
 
 const getUserSessionHandler = async (req: Request, res: Response, next: NextFunction) => {
+  console.log(res.locals);
   const userId = res.locals.user._id;
   return await findSessions({ user: userId, valid: true })
     .then((session) => {
--- server/src/api/routes/session.ts
+++ server/src/api/routes/session.ts
@@ -1,9 +1,12 @@
 import express from 'express';
 import { createUserSessionHandler, deleteSessionHandler, getUserSessionHandler } from '../controllers/SessionController';
+import { requireUser } from '../middleware/requireUser';
+import { validateResource } from '../middleware/validateResource';
+import { UserSchema } from '../validations/UserSchema';
 const router = express.Router();
 
-router.post('/', createUserSessionHandler);
-router.get('/', getUserSessionHandler);
-router.delete('/', deleteSessionHandler);
+router.post('/', validateResource(UserSchema.login), createUserSessionHandler);
+router.get('/', requireUser, getUserSessionHandler);
+router.delete('/', requireUser, deleteSessionHandler);
 
 export default router;
