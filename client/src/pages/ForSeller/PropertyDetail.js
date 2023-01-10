c576ec592e6dff1c4a4b2a4a1bdeb287674a038b
An Thuyen <anthuyen0206@gmail.com>
2023-01-10 17:49:48 +0700

    upd fe auction list for seller

--- client/src/pages/ForSeller/PropertyDetail.js
+++ client/src/pages/ForSeller/PropertyDetail.js
@@ -16,6 +16,8 @@ import HeaderUser from "../../components/header/HeaderUser";
 import Cookies from "js-cookie";
 import jwt from "jsonwebtoken";
 import Loading from "../../components/loading/Loading";
+import Time from "../../components/time/Time";
+import { useNavigate } from "react-router-dom";
 
 const PropertyDetail = () => {
     // const [date, setDate] = useState([
@@ -23,14 +25,17 @@ const PropertyDetail = () => {
     //   new DateObject().add(1, "month").setDay(15),
     // ]);
     const { id } = useParams();
+    const navigate = useNavigate();
 
     const baseURL = `http://localhost:8800/api/property/getById/${id}`;
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
     const [role, setRole] = useState();
-
+    const Cancel = () => {
+        navigate("/myProperty");
+    };
     useEffect(() => {
-        const fetchData = async()=>{
+        const fetchData = async () => {
             setLoading(true);
             await axios.get(baseURL).then((resp) => {
                 console.log(resp.data);
@@ -43,8 +48,8 @@ const PropertyDetail = () => {
                 setRole("");
             }
             setLoading(false);
-        }
-       fetchData();
+        };
+        fetchData();
     }, [baseURL]);
 
     const [viewPropertyTime, setViewPropertyTime] = useState([new DateObject().setDay(15), new DateObject().add(1, "month").setDay(15)]);
@@ -59,201 +64,118 @@ const PropertyDetail = () => {
         });
         return users;
     };
-    return loading ? (
+    return !loading ? (
         <Loading />
     ) : (
         <>
-            {(() => {
-                if (role === "BIDDER" || role === "SELLER" || role === "MANAGER" || role === "ADMIN") {
-                    return <HeaderUser username={getUser().userName} />;
-                } else {
-                    return <Header />;
-                }
-            })()}{" "}
-            <NavBar />
             <form>
                 <div className={styles.root}>
                     <SidebarSeller />
+                    <Time />
                     <div className={styles.info}>
                         <div>
                             <p className={styles.title}>Basic Information</p>
 
-                            <div className={styles.col1}>
-                                <p className={styles.lable}>Property Image</p>
-                                <p className={styles.lable}>Property Video</p>
-                                <br />
-                                <br />
-                                <br />
-                                <br />
-                                <br />
-                                <br />
-                                <br />
-                                <br />
-                                <br />
-                                <p className={styles.lable}>Property Name</p>
-                                <p className={styles.lable}>Category</p>
-                                <p className={styles.lable}>Start Bid</p>
-                                <p className={styles.lable}>Deposit</p>
-                                <p className={styles.lable}>Price Step</p>
-                                <p className={styles.lable}>Place View Property</p>
-                                <p className={styles.lable}>View Property Time</p>
-                                <p className={styles.lable}>Property Description</p>
+                            <div className={styles.fl}>
+                                <div className={styles.l}>
+                                    <p className={styles.lable}>Property Image</p>
+                                </div>
+                                <div className={styles.r}>
+                                    <img className={styles.img} src={`https://www.w3schools.com/html/pic_trulli.jpg`} alt="images" />
+                                    <img className={styles.img} src={`https://www.w3schools.com/html/pic_trulli.jpg`} alt="images" />
+                                    <img className={styles.img} src={`https://www.w3schools.com/html/pic_trulli.jpg`} alt="images" />
+                                </div>
                             </div>
-                            <div className={styles.col2}>
-                                {/* <img
-                  className={styles.img}
-                  src="https://www.w3schools.com/html/pic_trulli.jpg"
-                  alt="images"
-                /> */}
-                                <img
-                                    className={styles.img}
-                                    src={`http://localhost:8800/api/auction/images/${data.Properties_Media__r.records[0].Name}`}
-                                    alt="images"
-                                />
-                                {/* <img
-                  className={styles.img}
-                  src="https://www.w3schools.com/html/pic_trulli.jpg"
-                  alt="images"
-                /> */}
-                                <img
-                                    className={styles.img}
-                                    src={`http://localhost:8800/api/auction/images/${data.Properties_Media__r.records[1].Name}`}
-                                    alt="images"
-                                />
-                                <img
-                                    className={styles.img}
-                                    src={`http://localhost:8800/api/auction/images/${data.Properties_Media__r.records[2].Name}`}
-                                    alt="images"
-                                />
-                                <div className={styles.video}>
+                            <br />
+                            <div className={styles.fl}>
+                                <div className={styles.l}>
+                                    <p className={styles.lable}>Property Video</p>
+                                </div>
+                                <div className={styles.r}>
                                     <ReactPlayer
                                         className={styles.video}
-                                        url={data.Properties_Media__r.records[3].Name}
+                                        url={"https://www.youtube.com/watch?v=9l90STNZy_A"}
                                         playing={true}
                                         controls={true}
                                         loop={true}
                                         muted={true}
                                         playsinline={true}
                                         onReady={true}
-                                        width="85%"
+                                        width="40%"
                                         height="90%"
                                     />
                                 </div>
-                                {/* <img
-                  className={styles.img}
-                  src={`http://localhost:8800/api/auction/images/${data[0].Properties_Media__r.records[2].Name}`}
-                  alt="images"
-                /> */}
-                                {/* <input
-                  className={styles.inputImg}
-                  id="propertyImage"
-                  type="file"
-                  multiple
-                  required
-                ></input>
-                <br />
-                <input
-                  className={styles.inputImg}
-                  type="file"
-                  id="propertyVideo"
-                  required
-                  //   defaultValue={data.property.propertyVideo}
-                ></input> */}
-                                <input
-                                    id="propertyName"
-                                    type="text"
-                                    placeholder="Enter Property Name"
-                                    className={styles.inputText}
-                                    value={data.Name}
-                                    readonly
-                                ></input>
-                                <select
-                                    className={styles.drop}
-                                    id="cagetory"
-                                    placeholder="Category"
-                                    // value={data.Category_Id__r.Name}
-                                    // defaultValue={data.Category_Id__r.Name}
-                                    readonly
-                                >
-                                    <option value={data.Category_Id__r.Name}>{data.Category_Id__r.Name}</option>
-                                    {/* <option value={data.property.category}>{data.property.category}</option> */}
-
-                                    {/* {data.map((property) => (
-                  <option value={property.category}>{property.category}</option>
-                ))} */}
-                                </select>
-                                <input
-                                    id="startBid"
-                                    type="number"
-                                    placeholder="Enter Start Bid"
-                                    className={styles.inputText}
-                                    // value={startBid}
-                                    value={data.Start_Bid__c}
-                                    // value={"123"}
-                                    required
-                                ></input>
-                                <input
-                                    id="deposit"
-                                    type="text"
-                                    placeholder="Enter Deposit"
-                                    className={styles.inputText}
-                                    // value={deposit}
-                                    value={data.Deposit_Amount__c}
-                                    // value={"123"}
-                                    required
-                                    readonly
-                                ></input>
-                                <input
-                                    id="priceStep"
-                                    type="number"
-                                    placeholder="Enter Price Step"
-                                    className={styles.inputText}
-                                    // value={priceStep}
-                                    value={data.Price_Step__c}
-                                    // value={"321"}
-                                    required
-                                    readonly
-                                ></input>
-                                <input
-                                    id="placeViewProperty"
-                                    type="text"
-                                    placeholder="Enter Place View Property"
-                                    className={styles.inputText}
-                                    // value={placeViewProperty}
-                                    value={data.Place_View_Property__c}
-                                    // value={"Ha Noi"}
-                                    required
-                                    readonly
-                                ></input>
-                                <DatePicker
-                                    id="placeViewProperty"
-                                    // onChange={(e) => handleInputChange(e)}
-                                    onChange={setViewPropertyTime}
-                                    ClassName={styles.datePicker}
-                                    value={viewPropertyTime}
-                                    //   value={data.property.viewPropertyTime}
-                                    // onChange={setValue}
-                                    range
-                                    numberOfMonths={2}
-                                    format="MM/DD/YYYY HH:mm:ss"
-                                    plugins={[<TimePicker />]}
-                                    readOnly
-                                />
-                                <textarea
-                                    id="propertyDescription"
-                                    // value={propertyDescription}
-                                    value={data.Description__c}
-                                    // value={
-                                    //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                                    // }
-                                    className={styles.textarea}
-                                    required
-                                ></textarea>{" "}
+                            </div>
+                            <div className={styles.fl}>
+                                <div className={styles.l}>
+                                    <p className={styles.lable}>Property Name</p>
+                                </div>
+                                <div className={styles.r}>
+                                    <p className={styles.txt}>ABC XYZ</p>
+                                </div>
+                            </div>
+                            <div className={styles.fl}>
+                                <div className={styles.l}>
+                                    <p className={styles.lable}>Category</p>
+                                </div>
+                                <div className={styles.r}>
+                                    <p className={styles.txt}>ABC XYZ</p>
+                                </div>
+                            </div>
+                            <div className={styles.fl}>
+                                <div className={styles.l}>
+                                    <p className={styles.lable}>Start Bid</p>
+                                </div>
+                                <div className={styles.r}>
+                                    <p className={styles.txt}>ABC XYZ</p>
+                                </div>
+                            </div>
+                            <div className={styles.fl}>
+                                <div className={styles.l}>
+                                    <p className={styles.lable}>Deposit</p>
+                                </div>
+                                <div className={styles.r}>
+                                    <p className={styles.txt}>ABC XYZ</p>
+                                </div>
+                            </div>
+                            <div className={styles.fl}>
+                                <div className={styles.l}>
+                                    <p className={styles.lable}>Price Step</p>
+                                </div>
+                                <div className={styles.r}>
+                                    <p className={styles.txt}>ABC XYZ</p>
+                                </div>
+                            </div>
+                            <div className={styles.fl}>
+                                <div className={styles.l}>
+                                    <p className={styles.lable}>Place View Property</p>
+                                </div>
+                                <div className={styles.r}>
+                                    <p className={styles.txt}>ABC XYZ</p>
+                                </div>
+                            </div>
+                            <div className={styles.fl}>
+                                <div className={styles.l}>
+                                    <p className={styles.lable}>View Property Time</p>
+                                </div>
+                                <div className={styles.r}>
+                                    <p className={styles.txt}>ABC XYZ</p>
+                                </div>
+                            </div>
+                            <div className={styles.fl}>
+                                <div className={styles.l}>
+                                    <p className={styles.lable}>Property Description</p>
+                                </div>
+                                <div className={styles.r}>
+                                    <p className={styles.txt}>ABC XYZ</p>
+                                </div>
                             </div>
                         </div>
                     </div>
 
-                    <Footer />
+                    <div className={styles.btn}>
+                        <input className={styles.btnCancel} type="button" value="Cancel" onClick={Cancel}></input>
+                    </div>
                 </div>
             </form>
         </>
