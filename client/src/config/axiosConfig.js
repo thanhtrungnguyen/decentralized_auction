b692edce05f76409cc16f7247f45bb1e6b74cfa8
Nguyen Thanh Trung <thanhtrung692k@gmail.com>
2023-01-11 20:15:24 +0700

    update login, axios

--- client/src/config/axiosConfig.js
+++ client/src/config/axiosConfig.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const accessToken = localStorage.getItem("accessToken");
+const baseURL = process.env.REACT_APP_BASE_URL || "http://localhost:5000";
 
-axios.defaults.baseURL = process.env.REACT_APP_BASE_URL || "http://localhost:5000/api";
+axios.defaults.baseURL = `${baseURL}/api`;
 axios.defaults.headers.common = { Authorization: `Bearer  + ${accessToken}` };
 
 export default axios;
