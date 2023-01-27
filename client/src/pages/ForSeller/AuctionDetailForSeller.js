16a429fc92420f104acdafd55a5ad7a6ef7a32af
congtm20 <congtm20@gmail.com>
2023-01-27 22:46:54 +0700

    auction for seller

--- client/src/pages/ForSeller/AuctionDetailForSeller.js
+++ client/src/pages/ForSeller/AuctionDetailForSeller.js
@@ -11,10 +11,12 @@ import HeaderUser from "../../components/header/HeaderUser";
 import Loading from "../../components/loading/Loading";
 import Pagination from "@mui/material/Pagination";
 import Time from "../../components/time/Time";
+import moment from "moment";
+import "moment/locale/vi";
 const AuctionDetailForSeller = () => {
     const { id } = useParams();
     const navigate = useNavigate();
-    const baseURL = `http://localhost:8800/api/auction/auctiondetailForSeller/${id}`;
+    const baseURL = `/auction/${id}`;
     const { data, loading } = useFetch(baseURL);
     const [role, setRole] = useState();
 
@@ -36,7 +38,7 @@ const AuctionDetailForSeller = () => {
                         <div className={styles.col1}>
                             <div className={styles.col3}>
                                 <div className={styles.row}>
-                                    <img className={styles.img} src="https://www.w3schools.com/html/pic_trulli.jpg" alt="images" />
+                                    <img className={styles.img} src={data.auction.property.mediaUrl[0]} alt="images" />
                                     {/* <img
                     className={styles.img}
                     src={`http://localhost:8800/api/auction/images/${data[0].Properties_Media__r.records[0].Name}`}
@@ -44,7 +46,7 @@ const AuctionDetailForSeller = () => {
                   /> */}
                                 </div>
                                 <div className={styles.row}>
-                                    <img className={styles.img} src="https://www.w3schools.com/html/pic_trulli.jpg" alt="images" />
+                                    <img className={styles.img} src={data.auction.property.mediaUrl[1]} alt="images" />
                                     {/* <img
                     className={styles.img}
                     src={`http://localhost:8800/api/auction/images/${data[0].Properties_Media__r.records[1].Name}`}
@@ -52,7 +54,7 @@ const AuctionDetailForSeller = () => {
                   /> */}
                                 </div>
                                 <div className={styles.row}>
-                                    <img className={styles.img} src="https://www.w3schools.com/html/pic_trulli.jpg" alt="images" />
+                                    <img className={styles.img} src={data.auction.property.mediaUrl[2]} alt="images" />
                                     {/* <img
                     className={styles.img}
                     src={`http://localhost:8800/api/auction/images/${data[0].Properties_Media__r.records[2].Name}`}
@@ -63,7 +65,7 @@ const AuctionDetailForSeller = () => {
                             <div className={styles.col4}>
                                 <ReactPlayer
                                     className={styles.video}
-                                    url="https://www.youtube.com/watch?v=6ltIt-NtQjQ"
+                                    url={data.auction.property.mediaUrl[3]}
                                     playing={true}
                                     controls={true}
                                     loop={true}
@@ -80,19 +82,19 @@ const AuctionDetailForSeller = () => {
                             {/* <p className={styles.title}>{data.auction.propertyName}</p> */}
                             <br />
                             <br />
-                            <p className={styles.txtBlue}>Start bid : 300 ETH</p>
+                            <p className={styles.txtBlue}>Start bid : {data.auction.property.startBid} ETH</p>
                             {/* <p className={styles.txtBlue}>
                 Start bid : ${data.auction.startBid}
               </p> */}
-                            <p className={styles.txtBlue}>Cureent bid : $350 ETH</p>
+                            <p className={styles.txtBlue}>Current bid : {data.auction.property.startBid} ETH</p>
                             {/* <p className={styles.txtBlue}>
                 Cureent bid : ${data.auction.currentBid}
               </p> */}
-                            <p className={styles.txt}>Auction Start Time : 31/12/2022 9:00AM</p>
+                            <p className={styles.txt}>Auction Start Time : {moment(data.auction.property.startAuctionTime).format("L")}, {moment(data.auction.property.startAuctionTime).format("LTS")}</p>
                             {/* <p className={styles.txt}>
                 Auction Start Time : ${data.auction.startTime}
               </p> */}
-                            <p className={styles.txt}>Auction End Time : 31/12/2022 11:00AM</p>
+                            <p className={styles.txt}>Auction End Time : {moment(data.auction.property.endAuctionTime).format("L")}, {moment(data.auction.property.endAuctionTime).format("LTS")}</p>
                             {/* <p className={styles.txt}>
                 Auction End Time : ${data.auction.endTime}
               </p> */}
@@ -106,13 +108,13 @@ const AuctionDetailForSeller = () => {
                                 <th className={styles.th}>Time</th>
                                 <th className={styles.th}>Transaction Address</th>
                             </tr>
-                            {data.map((auction) => (
+                            {/* {data.map((auction) => (
                                 <tr>
                                     <td className={styles.td}>{auction.amout}</td>
                                     <td className={styles.td}>{auction.time}</td>
                                     <td className={styles.td}>{auction.transactionAddress}</td>
                                 </tr>
-                            ))}
+                            ))} */}
                             <tr>
                                 <td className={styles.td}>Dianne Russell</td>
                                 <td className={styles.td}>From 10:00-06/24/2021 to 10:00-06/24/2021</td>
@@ -141,9 +143,9 @@ const AuctionDetailForSeller = () => {
                         </table>
                         <Pagination
                             className={styles.Pagination}
-                            // count={data.total % 10 > 0 ? Math.floor(data.total / 10) + 1 : data.total / 10}
-                            // page={page}
-                            // onChange={handleChange}
+                        // count={data.total % 10 > 0 ? Math.floor(data.total / 10) + 1 : data.total / 10}
+                        // page={page}
+                        // onChange={handleChange}
                         />
                     </div>
                     <div className={styles.des}>
