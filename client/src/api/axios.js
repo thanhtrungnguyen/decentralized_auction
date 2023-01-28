c73899a0306863de37f8bd6d8977c4c6fb1235f9
Nguyen Thanh Trung <thanhtrung692k@gmail.com>
2023-01-28 15:45:34 +0700

    http://20.205.108.122:5000

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://20.205.108.122:5000";
+const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000";
 
 export default axios.create({
     baseURL: `${BASE_URL}/api`,
