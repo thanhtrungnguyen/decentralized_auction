e14ccc87e7e4674fd79e42007fcf54591be4b259
Nguyen Thanh Trung <thanhtrung692k@gmail.com>
2023-01-28 15:54:45 +0700

    http://20.205.108.122:3000/

--- server/src/config/allowed-origins.ts
+++ server/src/config/allowed-origins.ts
@@ -1,3 +1,9 @@
 import { config } from './custom-environment-variables';
 
-export const allowedOrigins = ['*', 'https://www.fasdgfsadfgfdgdf.com', 'http://127.0.0.1:3000', 'http://localhost:3000', config.client.address];
+export const allowedOrigins = [
+  'http://20.205.108.122:3000/',
+  'https://www.fasdgfsadfgfdgdf.com',
+  'http://127.0.0.1:3000',
+  'http://localhost:3000',
+  config.client.address
+];
