8fb60d201bbdf91d63df43b7b934eaeded98d0b8
Nguyen Thanh Trung <thanhtrung692k@gmail.com>
2023-01-28 16:13:54 +0700

    update

--- server/src/config/cors-options.ts
+++ server/src/config/cors-options.ts
@@ -2,7 +2,6 @@ import { allowedOrigins } from './allowed-origins';
 
 export const corsOptions = {
   // origin: allowedOrigins,
-  origin: '*',
   methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
   credentials: true,
   optionsSuccessStatus: 200
