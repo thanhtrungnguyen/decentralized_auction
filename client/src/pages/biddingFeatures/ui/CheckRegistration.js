dca89199647eff92f4fd3a579cc62d986bd3a405
Nguyen Thanh Trung <thanhtrung692k@gmail.com>
2023-02-02 23:25:00 +0700

    fix bid

---
+++ client/src/pages/biddingFeatures/ui/CheckRegistration.js
@@ -0,0 +1,28 @@
+import { useFetchData } from "../../../hooks/useFetch";
+import Loader from "../components/Loader";
+import styles from "../../../styleCss/stylesComponents/placeABid.module.css";
+import WaitingForAuctionTime from "./WaitingForAuctionTime";
+
+const CheckRegistration = ({ auction, property }) => {
+    const { loading, data, error } = useFetchData(`/auctionRegistration/user/${auction.auctionId}`);
+    if (loading) return <Loader />;
+    if (error)
+        return (
+            <div className={styles.notification}>
+                <p>You have not registered the auction.</p>
+            </div>
+        );
+    if (data?.auctionRegistration?.length === 0)
+        return (
+            <div className={styles.notification}>
+                <p>You have not registered the auction.</p>
+            </div>
+        );
+    return (
+        <div>
+            <WaitingForAuctionTime auction={auction} property={property} />
+        </div>
+    );
+};
+
+export default CheckRegistration;
