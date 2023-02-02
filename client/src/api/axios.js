2b7e2349852d99d7a9ed305282f99909a08b6319
Nguyen Thanh Trung <thanhtrung692k@gmail.com>
2023-02-02 21:22:51 +0700

    fix

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = process.env.REACT_APP_BASE_URL || "";
+export const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:5000";
 // export const BASE_URL = "";
 
 export default axios.create({
